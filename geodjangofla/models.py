953a1c18d3a090d7f1db4c1eba33f5856a9dff99
Étienne Loks <etienne.loks@peacefrogs.net>
2012-01-02 16:31:40 +0100

    Remove unecessary import statements

--- geodjangofla/models.py
+++ geodjangofla/models.py
@@ -9,7 +9,6 @@
 from geodjangofla import settings
 
 from django.contrib.gis.db import models
-from django.contrib.gis.geos import Point, GEOSGeometry
 from django.core.exceptions import ObjectDoesNotExist
 
 class GEOFLAManager:
