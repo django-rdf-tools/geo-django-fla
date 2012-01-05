a338ed4cb0bcd1b0726f46b175d97d7e131c04b0
Étienne Loks <etienne.loks@peacefrogs.net>
2012-01-05 19:54:18 +0100

    Add a setup file

---
+++ geodjangofla/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+VERSION = (0, 1)
+
+def get_version():
+    version = '%s.%s' % (VERSION[0], VERSION[1])
+    return version
+
+__version__ = get_version()
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    import ez_setup
+    ez_setup.use_setuptools()
+    from setuptools import setup, find_packages
+
+VERSION = __import__('geodjangofla').__version__
+
+import os
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='geodjangofla',
+    version = VERSION,
+    description='This software is a Django application to simplify management '\
+                'of GEOFLA(R) datas of the IGN (french geographic institute).',
+    packages=['geodjangofla',
+              'geodjangofla.management',
+              'geodjangofla.management.commands',
+              'geodjangofla.migrations'
+              ],
+    author=u'Étienne Loks',
+    author_email='etienne.loks@peacefrogs.net',
+    license='BSD',
+    long_description=read('README.rst'),
+    zip_safe=False,
+    install_requires = ['Django==1.2',
+                        'south==0.7.3',],
+)
+
+
